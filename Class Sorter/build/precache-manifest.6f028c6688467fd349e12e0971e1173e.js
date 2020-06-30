3988f04d7fc99ac992f0769df1f4f3f7a1d48d2a
Leo Liao <leo211liao@gmail.com>
2020-06-29 18:12:56 -0700

    sorter v1

---
+++ Class Sorter/build/precache-manifest.6f028c6688467fd349e12e0971e1173e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4243cee6d2cc01e092e794f8955b957c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec6cb39fce673b2fac97",
+    "url": "./static/css/main.543ab5e1.chunk.css"
+  },
+  {
+    "revision": "a3be8d66d78fceedf178",
+    "url": "./static/js/2.2c750f9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2c750f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6cb39fce673b2fac97",
+    "url": "./static/js/main.b7514217.chunk.js"
+  },
+  {
+    "revision": "de8611f185e6098f4a67",
+    "url": "./static/js/runtime-main.54108d54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
